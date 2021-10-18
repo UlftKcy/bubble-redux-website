bf09b395eac18d0779a0eab85a12d74a9a587460
UlfetKacay <ukacay87@gmail.com>
2021-10-19 02:13:36 +0300

    json server added

--- src/components/dashboard/Products.js
+++ src/components/dashboard/Products.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React ,{useEffect, useState} from 'react'
 import { Grid, Typography } from '@material-ui/core';
 import ShopProduct from './ShopProduct';
 
+
 const Products = () => {
+    
+    useEffect(() => {
+        fetch("http://localhost:3000/categories")
+        .then(response=>response.json())
+        .then(data=>console.log(data))
+    }, [])
+    
     return (
         <Grid container sx={{ flexGrow: 1 }}>
             <Grid item xs={12}>
