0954ab66f1d90cc9afbd0a61ddd29da54641e3e7
UlfetKacay <ukacay87@gmail.com>
2021-10-29 13:32:02 +0300

    banner updated

--- src/components/dashboard/Banner.js
+++ src/components/dashboard/Banner.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import { Card, CardMedia } from '@material-ui/core';
+import React from "react";
+import { Card, CardMedia } from "@material-ui/core";
 import banner from "../../assets/banner.png";
 
 const Banner = () => {
-    return (
-        <Card>
-            <CardMedia
-            component="img"
-            image={banner}
-            alt="banner"
-            />
-        </Card>
-    )
-}
+  return (
+    <Card>
+      <CardMedia
+        component="img"
+        image={banner}
+        alt="banner"
+        style={{ maxHeight: "75vh" }}
+      />
+    </Card>
+  );
+};
 
 export default Banner;
